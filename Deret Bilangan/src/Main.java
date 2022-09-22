02b3e3c8594e053cb192aaba6239f9fcc263c419
Renita Nathania <87638747+renitanathania@users.noreply.github.com>
2022-09-22 16:27:50 +0700

    Add files via upload
    Judul

---
+++ Deret Bilangan/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Deret Bilangan");
+        System.out.println("################\n");
+
+        System.out.println("20 Bilangan Genap :");
+
+    }
+}
